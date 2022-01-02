62d1b322f53f99456a37f463a7eadca699d4e195
mostafabashir <47763564+mostafabashir@users.noreply.github.com>
2022-01-02 16:00:50 -0600

    Update 3.atoi.c
    Proposing changes to give appropriate value in decimal form. Current solution is the decimal converter included as an example immediately below 2.7 in the text but does not edit the example to address hex inputs.

--- chapter-2-types-operators-expressions/3.atoi.c
+++ chapter-2-types-operators-expressions/3.atoi.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 int atoi(char[]);
+int htoi(char[]);
 
 main()
 {
 	int a = atoi("1234567890");
 	printf("%d\n", a);
 	a = atoi("1234abc567890");
+	printf("%d\n", a);
+	
+	//repeat lines 10 and 11 using htoi
+	a = htoi("1234abc567890");
 	printf("%d\n", a);
 }
 
@@ -18,3 +23,31 @@ int atoi(char s[])
 		n = 10 * n + (s[i] - '0');
 	return n;
 }
+
+/*
+Function htoi should return hexadecimal form. Current function atoi returns only decimal form
+For example, line 10 (originally at line 9) returns a value of 1234 rather than the correct decimal value of 320278871046288.
+See function below. In current form, return is an int, can also be a long double to support larger inputs
+*/
+
+int htoi(char s[])
+{
+	int i;
+	long double n;
+
+	n = 0;
+	for (i = 0; (s[i] >= '0' && s[i] <= '9') || (s[i] >= 'a' && s[i] <= 'f') || (s[i] >= 'A' && s[i] <= 'F'); ++i)
+	{
+		if (i == 0 && s[i] == '0')
+			i = 2;
+		else if (s[i] >= '0' && s[i] <= '9')
+			n = 16 * n + (s[i] - '0');
+		else if (s[i] >= 'a' && s[i] <= 'f')
+			n = 16 * n + (s[i] - 'a' + 10.0);
+		else
+			n = 16 * n + (s[i] - 'A' + 10.0);
+	}
+	return n;
+}
+
+
